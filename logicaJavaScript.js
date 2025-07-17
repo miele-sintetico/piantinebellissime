b57aeac6d7c4cc5c74f9cebe97b8ee17078082cc
miele-sintetico <rosalice247@gmail.com>
2025-07-17 23:03:47 +0200

    1

---
+++ logicaJavaScript.js
@@ -0,0 +1,16 @@
+// Questo è un codice Javascript. Tutto quello che si trova dopo i doppi slash "//" non viene contato
+// come "programma" ma come testo a scopo umano e viene colorato di verde in quasi
+// tutti i programmi per scrivere codice. 
+
+// Questi qui sotto sono funzioni javascript. Le funzioni possono essere "definite" e "richiamate".
+// Quando le scrivi descrivono cosa fanno. Quando le richiami quello che hai scritto nella definizione 
+// sono una serie di istruzioni che vengono eseguite ogni volta che richiami la funzione.
+
+//Qui sotto ho definito due funzioni che cambiano l'indirizzo della pagina con quello che gli dici.
+function linkCheTiPortaAPagina2(){
+    location.href="/pagina2.html"
+}
+
+function linkCheTiPortaAPagina3(){
+    location.href="/pagina3/" //
+}
